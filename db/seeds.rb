a4eb1ec5feb12f9118c7e722ce6bde7c488cc8ab
Nelly Duclos <nellyduclos8@gmail.com>
2018-09-04 18:36:19 +0200

    ajoute les seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.destroy_all
+
+@user = User.create!(username: 'admin',  password: 'password', password_confirmation: 'password')
+
+
+FinishedGuitar.destroy_all
+
+puts 'Created user'
+
+counter = 0
+10.times do 
+	counter += 1
+FinishedGuitar.create!( 
+	user_id: @user.id,
+	title:  Faker::Music.instrument,
+	description: Faker::Lorem.paragraph(2),	
+	:attachments_attributes => {
+		0 => {
+			image:  File.new(Rails.root.join('app/assets/images/seeds/', 'image_1.jpg')),
+		},
+		1 => {
+			image:  File.new(Rails.root.join('app/assets/images/seeds/', 'image_2.jpg')),
+		},
+		2 => {
+			image:  File.new(Rails.root.join('app/assets/images/seeds/', 'image_3.jpg')),
+		},
+		3 => {
+			image:  File.new(Rails.root.join('app/assets/images/seeds/', 'image_4.jpg')),
+		}
+	}
+		)
+
+
+puts 'Created Finished guitar ' + counter.to_s
+
+end
