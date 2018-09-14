e8fa568a4f596959255303cc7f0da6251cfeaaef
Nelly Duclos <nellyduclos8@gmail.com>
2018-09-14 22:03:12 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,124 +13,35 @@ puts 'Created user'
 3.times do 
 	
 FinishedGuitar.create!( 
-	title:  Faker::Music.instrument,
-	description: Faker::Lorem.paragraph(3),	
-	attachments: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg")
-								].shuffle)
-puts 'Created Finished guitars '
-
-end
-
-
-6.times do 
-	
-	FinishedGuitar.create!(
 		title:  Faker::Music.instrument,
 		description: Faker::Lorem.paragraph(3),	
-		attachments: [
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_4.jpg"),
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_5.jpg"),
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_6.jpg")
-	].shuffle)
-
-	puts 'Created Finished guitar '
-
+		attachments: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
+										open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
+										open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg")].shuffle)
+	puts 'Created Finished guitars'
 end
 
 
-6.times do 
-	
-	FinishedGuitar.create!( 
-	title:  Faker::Music.instrument,
-	description: Faker::Lorem.paragraph(3),	
-	attachments: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_7.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_8.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_9.jpg")
-								].shuffle)
-puts 'Created Finished guitars '
-
-end
 
 
-6.times do 
-	
-	FinishedGuitar.create!(
+3.times do 	
+	Progress.create!( 
 		title:  Faker::Music.instrument,
-		description: Faker::Lorem.paragraph(3),	
-		attachments: [
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_10.jpg"),
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
-		open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_5.jpg")
-	].shuffle)
-
-	puts 'Created Finished guitar ' 
-
-end
-
-
-6.times do 
-	
-Progress.create!( 
-	title:  Faker::Music.instrument,
-	description: Faker::Lorem.paragraph(2),	
-	pictures: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_4.jpg")
-							].shuffle)
-
-
-puts 'Created Progresses '
-
-end
-
-
-6.times do 
-	
-Progress.create!( 
-	title:  Faker::Music.instrument,
-	description: Faker::Lorem.paragraph(2),	
-	pictures: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_5.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_6.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_7.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_8.jpg")
-							].shuffle)
-
-
-puts 'Created Progresses ' 
-
-end
-
-
-6.times do 
-	
-Progress.create!( 
-	title:  Faker::Music.instrument,
-	description: Faker::Lorem.paragraph(2),	
-	pictures: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_9.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_10.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
-									open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg")
-							].shuffle)
-
-
-puts 'Created Progresses ' 
-
+		description: Faker::Lorem.paragraph(2),	
+		pictures: [  open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
+										open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
+										open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg"),
+										open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_4.jpg")].shuffle)
+	puts 'Created Progresses'
 end
 
 
 
 
-20.times do 
-	
-Video.create!( 
-	url: ["https://www.youtube.com/watch?v=NWdOz9IUGS0","https://www.youtube.com/watch?v=X8zJzBjmrng","https://www.youtube.com/watch?v=Ay-VsCI_4Oc", "https://www.youtube.com/watch?v=R9JOOoqj5P4"].sample ,
-	description: Faker::Lorem.paragraph(3)	
-	)
-
-
-puts 'Created Videos '
-
+3.times do 	
+	Video.create!( 
+		url: ["https://www.youtube.com/watch?v=NWdOz9IUGS0","https://www.youtube.com/watch?v=X8zJzBjmrng","https://www.youtube.com/watch?v=Ay-VsCI_4Oc", "https://www.youtube.com/watch?v=R9JOOoqj5P4"].sample,
+		description: Faker::Lorem.paragraph(3)	
+		)
+	puts 'Created Videos'
 end
