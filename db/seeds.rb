da8f16cbe28fac85eef7232c2ee3887a0328c040
Nelly Duclos <nellyduclos8@gmail.com>
2018-09-16 16:16:20 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,63 +3,105 @@
 # @user = User.create!(username: 'admin',  password: 'password', password_confirmation: 'password')
 
 
-FinishedGuitar.destroy_all
-Progress.destroy_all
-Video.destroy_all
+# FinishedGuitar.destroy_all
+# Progress.destroy_all
+# Video.destroy_all
 
-20.times do 
-	FinishedGuitar.create!( 
-		title:  Faker::Music.instrument,
-		description: Faker::Lorem.paragraph(10),	
-		attachments: 
-		 [  Rails.root.join("app/assets/images/seeds/image_1.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_2.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_3.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_4.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_5.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_6.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_7.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_8.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_9.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_10.jpg").open,
-				].shuffle)
-	print "_"
-end
-
-puts 'Created Finished guitars '
-
-
-20.times do 
-	Progress.create!( 
-		title:  Faker::Music.instrument,
-		description: Faker::Lorem.paragraph(10),	
-		pictures: 
-		 [  Rails.root.join("app/assets/images/seeds/image_1.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_2.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_3.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_4.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_5.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_6.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_7.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_8.jpg").open,
-				Rails.root.join("app/assets/images/seeds/image_9.jpg").open, 
-				Rails.root.join("app/assets/images/seeds/image_10.jpg").open,
-				].shuffle)
-	print "_"
-end
-
-puts 'Created Work in progress '
-
+# puts 'Created user'
 
 20.times do 
 	
-Video.create!( 
-	url: ["https://www.youtube.com/watch?v=NWdOz9IUGS0","https://www.youtube.com/watch?v=X8zJzBjmrng","https://www.youtube.com/watch?v=Ay-VsCI_4Oc", "https://www.youtube.com/watch?v=R9JOOoqj5P4"].sample ,
-	description: Faker::Lorem.paragraph(3)	
-	)
-print "_"
-
-
+	FinishedGuitar.create!( 
+			title:  Faker::Music.instrument,
+			description: Faker::Lorem.paragraph(3),	
+			attachments: [  
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg"), 
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_4.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_5.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_6.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_7.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_8.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_9.jpg"),
+				open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_10.jpg")])
 end
-puts 'Created Videos '
+
+
+
+
+# 20.times do 	
+# 	Progress.create!( 
+# 		title:  Faker::Music.instrument,
+# 		description: Faker::Lorem.paragraph(2),	
+# 		pictures: [ open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_1.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_2.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_3.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_4.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_5.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_6.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_7.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_8.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_9.jpg"),
+# 								open("https://s3-eu-west-1.amazonaws.com/leschnerguitars/images_site/seeds/image_10.jpg")].shuffle)
+# 	puts 'Created Progresses'
+# end
+
+
+
+# 20.times do 
+# 	FinishedGuitar.create!( 
+# 		title:  Faker::Music.instrument,
+# 		description: Faker::Lorem.paragraph(10),	
+# 		attachments: 
+# 		 [  Rails.root.join("app/assets/images/seeds/image_1.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_2.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_3.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_4.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_5.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_6.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_7.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_8.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_9.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_10.jpg").open,
+# 				].shuffle)
+# 	print "_"
+# end
+
+# puts 'Created Finished guitars '
+
+
+# 20.times do 
+# 	Progress.create!( 
+# 		title:  Faker::Music.instrument,
+# 		description: Faker::Lorem.paragraph(10),	
+# 		pictures: 
+# 		 [  Rails.root.join("app/assets/images/seeds/image_1.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_2.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_3.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_4.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_5.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_6.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_7.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_8.jpg").open,
+# 				Rails.root.join("app/assets/images/seeds/image_9.jpg").open, 
+# 				Rails.root.join("app/assets/images/seeds/image_10.jpg").open,
+# 				].shuffle)
+# 	print "_"
+# end
+
+# puts 'Created Work in progress '
+
+
+# 20.times do 
+	
+# Video.create!( 
+# 	url: ["https://www.youtube.com/watch?v=NWdOz9IUGS0","https://www.youtube.com/watch?v=X8zJzBjmrng","https://www.youtube.com/watch?v=Ay-VsCI_4Oc", "https://www.youtube.com/watch?v=R9JOOoqj5P4"].sample ,
+# 	description: Faker::Lorem.paragraph(3)	
+# 	)
+# print "_"
+
+
+# end
+# puts 'Created Videos '
 
