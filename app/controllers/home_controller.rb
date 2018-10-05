890825cc4f8c93ff9bf73502f1861ddffa0d1b2c
Nelly Duclos <nellyduclos8@gmail.com>
2018-10-05 15:51:12 +0200

    clean

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
 
   def index
   	@customization = Customization.instance
-  	set_meta_tags site: 'Leschner Guitars',
-  	              description: @customization.about,
-  	              keywords: ['Ivan Leschner', 'Luthier Buenos Aires', 'Argentina', 'Luthier Leschner']
   end
 
   def documentation
